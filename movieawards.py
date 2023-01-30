53fd66dc154a294f8a1d00f0108b44a31b859050
sharatd <sharat.lakers24@gmail.com>
2023-01-29 18:51:34 -0600

    add movieawards class

---
+++ movieawards.py
@@ -0,0 +1,20 @@
+class AwardFrame:
+
+    def __init__(self):
+        self.results = {}
+        self.nominee_type_system = {}
+        self.nomineeregex = []
+        self.winnerregex = []
+        self.awardsregex = []
+
+    def generate_awards(self, autofill = True):
+        pass
+
+    def generate_nominees(self, autofill = True):
+        pass
+
+    def type_system_nominee(self):
+        pass
+
+    def generate_winners(self):
+        pass 
