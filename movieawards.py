681ec0123d0be2c77b0b69fb0bcba486234eda3a
sharatd <sharat.lakers24@gmail.com>
2023-01-29 21:01:20 -0600

    find nominees

--- movieawards.py
+++ movieawards.py
@@ -7,17 +7,17 @@ def main():
     frame.generate_awards()
 
     frame.type_system_nominee()
-    frame.generate_nominees()
+    frame.generate_nominees(False)
 
-    frame.generate_winners()
+    #frame.generate_winners()
 
-    print(frame.results)
+    #print(frame.results)
 
 class AwardFrame:
     def __init__(self):
         self.results = {}
         self.nominee_type_system = {}
-        self.nominee_regex = []
+        self.nominee_regex = ["nominees for(.*) are(.*)", "(.*)is nominated for(.*)", "(.*)is[ ]?(?:a)?[ ]?nominee for(.*)", "(.*)are[ ]?(?:the)?[ ]?nominees for(.*)"]
         self.winner_regex = []
         self.awards_regex = ["(.*)goes to(.*)", "(.*)wins(.*)", "(.*)takes home(.*)", "(.*)receives(.*)", "(.*)is the winner of(.*)"]
 
@@ -43,7 +43,54 @@ class AwardFrame:
                 self.results[award]["nominees"] = nominee_list
         else:
             # TODO: Regex + type checking system
-            pass
+            results = {award : {} for award in self.results.keys()}
+            with open("./data/gg2013.json") as f:
+                tweets = json.load(f)
+
+            tweets = list(map(lambda x: x["text"], tweets))
+
+            for index, regex in enumerate(self.awards_regex):
+                for tweet in tweets:
+                    match = re.search(regex, tweet)
+
+                    if match:
+                        x, y = "", ""
+                        if index == 0:
+                            y, x = match.group(1), match.group(2)
+                        else:
+                            x, y = match.group(1), match.group(2)
+
+                        
+                        curr_award = None
+
+                        for award in self.results.keys():
+                            if award in y.lower():
+                                curr_award = award
+                                break
+                        if curr_award:
+                            if index == 0 or index == 3:
+                                if "," in x:
+                                    nominees = [s.strip() for s in x.split(",")]
+                                    for nominee in nominees:
+                                        if nominee in results[curr_award]:
+                                            results[curr_award][nominee] = results[curr_award][nominee] + 1
+                                        else:
+                                            results[curr_award][nominee] = 1 
+                            else:
+                                x = x.lower().strip()
+                                if x in results[curr_award]:
+                                    results[curr_award][x] = results[curr_award][x] + 1
+                                else:
+                                    results[curr_award][x] = 1 
+           
+            for award in results:
+                votes = sorted(results[award], key = lambda x: x[1], reverse = True)
+                self.results[award]["nominees"] = votes[:5]
+
+          
+
+
+
 
     def type_system_nominee(self):
         pass
